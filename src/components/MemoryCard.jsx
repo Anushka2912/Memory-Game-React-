73cfec022952cf13bce3ad51bb10197b4bb0b20f
Anushka Sharma <anushka626sata@gmail.com>
2025-02-15 15:29:05 -0500

    receive selected cards and matched cards data in memory card component. Store the selected and matched cards data in their respective variables

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,18 +1,24 @@
 import { decodeEntity } from "html-entities";
 import EmojiButton from "./EmojiButton";
 
-export default function MemoryCard({ handleClick, data }) {
+export default function MemoryCard({ handleClick, data, selectedCards, matchedCards }) {
     
-    const cardEl = data.map((emoji, index) =>
-        <li key={index} className="card-item">
-            <EmojiButton
-            content={decodeEntity(emoji.htmlCode[0])}
-                style="btn btn--emoji"
-                handleClick={() => handleClick(emoji.name, index)}
-            >
-            </EmojiButton>
-        </li>
-    )
+    const cardEl = data.map((emoji, index) => {
+
+        const selectedCardEntry = selectedCards.find(emoji => emoji.index === index);
+        const matchedCardEntry = matchedCards.find(emoji => emoji.index === index);
+
+        return (
+            <li key={index} className="card-item">
+                <EmojiButton
+                    content={decodeEntity(emoji.htmlCode[0])}
+                    style="btn btn--emoji"
+                    handleClick={() => handleClick(emoji.name, index)}
+                >
+                </EmojiButton>
+            </li>
+        )
+    })
     
     return <ul className="card-container">{cardEl}</ul>
 }
