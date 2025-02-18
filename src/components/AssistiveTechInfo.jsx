298c17c0106c0fc264b2183eaf0c223aa025e2e2
Anushka Sharma <anushka626sata@gmail.com>
2025-02-17 19:25:00 -0500

    Fix element attribute:  from class to className

--- src/components/AssistiveTechInfo.jsx
+++ src/components/AssistiveTechInfo.jsx
@@ -1,7 +1,7 @@
 export default function AssistiveTechInfo ({ emojisData, matchedCards }) {
 
     return (
-        <section class="sr-only" aria-live="polite" aria-atomic="true">
+        <section className="sr-only" aria-live="polite" aria-atomic="true">
             <h2>Game Status</h2>
             <p>Number of matched pairs: {matchedCards.length / 2}</p>
             <p>Number of cards left to match: {emojisData.length - matchedCards.length}</p>
