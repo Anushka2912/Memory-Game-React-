1f21c0f932593d8b8a9484695482a7323e06a441
Anushka Sharma <anushka626sata@gmail.com>
2025-02-18 00:18:03 -0500

    refactor: refactor the app to use dynamic data for category of cards and number of cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,18 @@ import GameOver from './components/GameOver';
 import ErrorCard from './components/ErrorCard';
 
 export default function App() {
+    const initialFormData = {
+        category: "animals-and-nature",
+        number: 10,
+    }
+
+    const [formData, setFormData] = useState(initialFormData);
     const [isGameOn, setIsGameOn] = useState(false);
     const [emojisData, setEmojisData] = useState([]);
     const [selectedCards, setSelectedCards] = useState([]);
     const [matchedCards, setMatchedCards] = useState([]);
     const [areAllCardsMatched, setAreAllCardsMatched] = useState(false);
     const [isError, setIsError] = useState(false);
-
-    console.log({isError});
 
     useEffect(() => {
         if (selectedCards.length === 2 && selectedCards[0].name === selectedCards[1].name) {
@@ -31,7 +35,8 @@ export default function App() {
     async function startGame(e) {
         e.preventDefault()
         try {
-            const response = await fetch("https://emojihub.yurace.pro/api/all/category/animals-and-nature");
+            const response = await fetch(`https://emojihub.yurace.pro/api/all/category/${formData.category}`);
+            
             if(response.ok) {
                 const data = await response.json();
                 const dataSlice = await getDataSlice(data);
@@ -63,8 +68,8 @@ export default function App() {
 
     function getRandomIndices(data) {
         const randomIndicesArray = [];
-
-        for(let i = 0; i < 5; i++) {
+   
+        for(let i = 0; i < formData.number / 2; i++) {
             const randomNum = Math.floor(Math.random() * data.length);
             if(!randomIndicesArray.includes(randomNum)) {
                 randomIndicesArray.push(randomNum);
