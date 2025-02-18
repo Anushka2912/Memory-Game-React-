2dbc98bbdf9952b331151d8c4517c245e631117c
Anushka Sharma <anushka626sata@gmail.com>
2025-02-18 00:25:25 -0500

    Add select elements in form component to select category and number fo memory cards

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,10 +2,32 @@ import RegularButton from './RegularButton'
 
 export default function Form({ handleSubmit }) {
     return (
-        <form className="wrapper">
-            <RegularButton handleClick={handleSubmit}>
-                Start Game
-            </RegularButton>
-        </form>
+        <div className="form-container">
+            <form className="wrapper">
+                <div className="form__inner-wrapper">
+                    <label for="category">Choose a category for memory cards</label>
+                    <select name='category' id='category' onChange={handleChange}>
+                        <option>animals and nature</option>
+                        <option>food and drink</option>
+                        <option>travel and places</option>
+                        <option>objects</option>
+                        <option>symbols</option>
+                    </select>
+                </div>
+                <div className="form__inner-wrapper">
+                    <label for="number">Select number of memory cards</label>
+                    <select name='number' id='number' onChange={handleChange}>
+                        <option>10</option>
+                        <option>20</option>
+                        <option>30</option>
+                        <option>40</option>
+                        <option>50</option>
+                    </select>
+                </div>
+                <RegularButton handleClick={handleSubmit}>
+                    Start Game
+                </RegularButton>
+            </form>
+        </div>
     )
 }
